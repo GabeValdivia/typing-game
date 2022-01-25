b3eaedee5e9fbcfb397c0c5b83d78cc48b6f3bfa
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-24 19:43:10 -0600

    Add array with list of words for game

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const settingsBtn = document.getElementById('setting-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficutlySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+	'sigh',
+	'tense',
+	'airplane',
+	'ball',
+	'pies',
+	'juice',
+	'warlike',
+	'bad',
+	'north',
+	'dependent',
+	'steer',
+	'silver',
+	'highfaltin',
+	'superficial',
+	'quice',
+	'eight',
+	'feeble',
+	'admit',
+	'drage',
+	'loving',
+];
